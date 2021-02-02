5353d5a87ec1abbe0c82853e6e7eab3026041b1f
dcoutinho1328 <60111446+dcoutinho1328@users.noreply.github.com>
2021-02-02 19:10:31 -0300

    Add files via upload

---
+++ Certificados/script.js
@@ -0,0 +1,25 @@
+function mover(elemento){
+    elemento.style.fontWeight="bolder";
+    elemento.style.backgroundColor="rgba(151, 255, 220, 0.685)"
+}
+
+function sair(elemento){
+    elemento.style.fontWeight="normal";
+    elemento.style.backgroundColor="rgb(0,0,0,0)"
+}
+
+function entrar_but(elemento){
+    elemento.style.backgroundColor=" rgb(80, 238, 225)"
+}
+
+function sair_but(elemento){    
+    elemento.style.backgroundColor="rgb(195, 207, 207)"
+}
+
+function mover_link(elemento){
+    elemento.style.fontWeight="bolder";
+}
+
+function sair_link(elemento){
+    elemento.style.fontWeight="normal";
+}
